7af64e8831ea39c41b491b7ae74a3296b277dbd6
phteven <skgough@gmail.com>
2024-02-02 00:08:57 -0600

    rename database file

--- src/build.rb
+++ src/build.rb
@@ -1,8 +1,7 @@
 require 'sqlite3'
 require 'erb'
-require 'pp'
 
-SQLite3::Database.new('music.db') do |db|
+SQLite3::Database.new('tunes.db') do |db|
   username =  db.execute('select * from users').flatten[1]
   tracks = db.execute('select * from tracks')
   template = ERB.new(File.read('template.html.erb'))
