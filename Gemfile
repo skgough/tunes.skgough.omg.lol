58736b44dba004020f30be436f7681e9acf39b34
phteven <skgough@gmail.com>
2024-02-02 23:05:58 -0600

    sinatra service for CMS MVP

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sqlite3'
+gem 'sinatra'
+
+gem "rackup", "~> 2.1"
---
+++ cms/cms.rb
@@ -0,0 +1,73 @@
+require 'sinatra'
+require 'sqlite3'
+require 'erb'
+require 'ostruct'
+
+def structify(query_result)
+  query_result ||= []
+  return query_result.map{ |u| OpenStruct.new(u.transform_keys(&:to_sym)) }
+end
+
+def init_db
+  conn = SQLite3::Database.open('tunes.db')
+  conn.results_as_hash = true
+  init_schema = File.read('schema.sql')
+  conn.execute(init_schema)
+  return conn
+end
+
+def rebuild_from(db)
+  username = structify(db.execute('select * from users')).first[:username]
+  tracks = structify(db.execute("select * from tracks order by created_at desc"))
+  template = ERB.new(File.read('tunes.html.erb'))
+  html = template.result(binding)
+  File.write('../build/index.html', html)
+end
+
+set :public_folder, __dir__
+
+get '/' do
+  db = init_db
+
+  user = structify(db.execute('select * from users')).first
+  tracks = structify(db.execute("select * from tracks"))
+
+  template = ERB.new(File.read('cms.html.erb'))
+  template.result(binding)
+end
+
+post '/user' do
+  db = init_db
+  update = <<-SQL
+    update users 
+    set username = ?,
+        api_key = ?
+    where users.id = 1
+  SQL
+  db.execute(update, [params['username'], params['api_key']])
+  rebuild_from db
+
+  redirect to '/'
+end
+
+post '/track' do
+  db = init_db
+  create = <<-SQL
+    insert into tracks
+    values (?,?,?,?,?,?)
+  SQL
+  db.execute(
+    create, 
+    [ 
+      nil,
+      params['yt_id'],
+      params['title'],
+      params['artist'],
+      nil,
+      Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    ]
+  )
+  rebuild_from db
+
+  redirect to '/'
+end
